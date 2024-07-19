01fbeb223506c7b0fdbe246b80def0d17178e31f
achampaud <achampaud@atakama-technologies.com>
2024-07-19 09:40:57 +0200

    wip : add export java8 files and script

--- overloading/export_modified_files.sh
+++ overloading/export_modified_files.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Le hash du commit Java 8
+# Définir les variables
+REPO_URL="https://github.com/atakama/VABF-petclinic-copy"
 JAVA8_COMMIT="9ecdc1111e3da388a750ace41a125287d9620534"
-# Le hash du dernier commit
 LAST_COMMIT="origin/HEAD"
-# Répertoire où stocker les fichiers modifiés
-OUTPUT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+OUTPUT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/java8
 
 # Créer le répertoire de sortie
 mkdir -p $OUTPUT_DIR
@@ -13,10 +12,10 @@ mkdir -p $OUTPUT_DIR
 # Obtenir la liste des fichiers modifiés
 files=$(git diff --name-only $JAVA8_COMMIT $LAST_COMMIT)
 
-# Copier les fichiers modifiés dans le répertoire de sortie
+# Télécharger chaque fichier modifié depuis GitHub
 for file in $files; do
   # Créer les sous-répertoires nécessaires
   mkdir -p $OUTPUT_DIR/$(dirname $file)
-  # Copier le fichier modifié
-  cp $file $OUTPUT_DIR/$file
+  # Télécharger le fichier modifié depuis le dépôt GitHub
+  wget -qO $OUTPUT_DIR/$file $REPO_URL/raw/$JAVA8_COMMIT/$file
 done
